bd217870a49a621c570eb42afa06a1a16a5b2e76
Paulo McNally <paulomcnally@gmail.com>
2014-10-27 23:26:02 -0600

    validate LOOPBACK_AUTO environment

--- server/boot/user.js
+++ server/boot/user.js
@@ -1,32 +1,34 @@
 module.exports = function userInit(server) {
   if (process.env.NODE_ENV !== 'production') {
-    var User = server.models.User;
+    if (!process.env.LOOPBACK_AUTO) {
+      var User = server.models.User;
 
-    //insert row on model RoleMapping
-    function addUser(username, email, password) {
-      User.findOne({
-        where: {
-          'username': username,
-          'email': email
-        }
-      }, function(err, res) {
-        if (res === null) {
-          User.create({
-            username: username,
-            email: email,
-            password: password
-          }, function(err, user) {
-            if (err) {
-              console.log(err);
-            }
-            else {
-              console.log('✓ User ' + user.username + ' created!');
-            }
-          });
-        }
-      });
+      //insert row on model RoleMapping
+      function addUser(username, email, password) {
+        User.findOne({
+          where: {
+            'username': username,
+            'email': email
+          }
+        }, function(err, res) {
+          if (res === null) {
+            User.create({
+              username: username,
+              email: email,
+              password: password
+            }, function(err, user) {
+              if (err) {
+                console.log(err);
+              }
+              else {
+                console.log('✓ User ' + user.username + ' created!');
+              }
+            });
+          }
+        });
+      }
+
+      addUser('paulomcnally', 'paulomcnally@gmail.com', '123456');
     }
-
-    addUser('paulomcnally', 'paulomcnally@gmail.com', '123456');
   }
 };
